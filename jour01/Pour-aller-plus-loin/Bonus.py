d19db6ec713639df3c9d45834a9bbfd618bd741f
alexandre-viale1 <115146018+alexandre-viale1@users.noreply.github.com>
2023-01-02 13:07:34 +0100

    projet runtrack python j1

---
+++ jour01/Pour-aller-plus-loin/Bonus.py
@@ -0,0 +1,7 @@
+string = input("Anton Voyl n'arrivait pas à dormir. Il alluma. Son Jaz marquait minuit vingt. Il poussa un profond soupir, s'assit dans son lit, s'appuyant sur son polochon.")
+
+if 'e' in string:
+
+    print("La chaîne de caractères contient le caractère 'e'.")
+else:
+    print("La chaîne de caractères ne contient pas le caractère 'e'.")
