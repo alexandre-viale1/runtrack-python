0ce624f47b127d0917ef7ea254382efee767f7ee
alexandre-viale1 <115146018+alexandre-viale1@users.noreply.github.com>
2023-01-03 17:55:42 +0100

    bonus j2

--- jour02/pour_aller_plus_loin/bonus.py
+++ jour02/pour_aller_plus_loin/bonus.py
@@ -1,16 +1,16 @@
 def triangle_type(a, b, c):
   if a + b > c and a + c > b and b + c > a:
     if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
-        return "triangle rectangle"
+        print("triangle rectangle") 
     elif a == b and b == c:
-        return "triangle equilatéral"
+        print("triangle equilatéral") 
     else:
         if a == b or a == c or b == c:
-            return "triangle isosèle"
+            print("triangle isosèle") 
         else:
-            return "triangle quelconque"
+            print("triangle quelconque") 
   else:
-    return "pas un triangle"
+    print("pas un triangle") 
 
 
-print(triangle_type(10,20,31))
+print(triangle_type(10,20,2))
